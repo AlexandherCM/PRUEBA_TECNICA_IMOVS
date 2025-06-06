88ebf6cf6ffef12ba14916634dd772ae9fcbc7e9
unknown <alcloponso@gmail.com>
2025-06-06 13:40:07 -0600

    Correccion

--- PRUEBA_TECNICA_IMOVS/PRUEBA_TECNICA_IMOVS/App_Start/WebApiConfig.cs
+++ PRUEBA_TECNICA_IMOVS/PRUEBA_TECNICA_IMOVS/App_Start/WebApiConfig.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http.Headers; // Necesario para MediaTypeHeaderValue
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace PRUEBA_TECNICA_IMOVS
